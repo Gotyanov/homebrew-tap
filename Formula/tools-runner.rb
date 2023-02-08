0428dd32e6742873d26e36b48bbb2879d951b38f
Gotyanov <gotyanovas@yandex-team.ru>
2023-02-08 16:40:30 +0300

    Update tools-runner.rb

--- Formula/tools-runner.rb
+++ Formula/tools-runner.rb
@@ -1,9 +1,9 @@
 class ToolsRunner < Formula
   desc "Helper tool for fething and running remote executables."
   homepage "https://github.com/Gotyanov/tools-runner"
-  url "https://github.com/Gotyanov/tools-runner/archive/refs/tags/0.4.0.tar.gz"
+  url "https://github.com/Gotyanov/tools-runner/archive/refs/tags/0.5.0.tar.gz"
 
-  sha256 "29bb093d17af465b998f3c3574ec2140f2b3057690244d340e5f00ed7fc07472"
+  sha256 "3b5a39d426c6ad385d785d4a4a178fc99b4cffb60493b518b0adfe3cdcbe7288"
 
   def install
     bin.install 'Prebuilt/tools'
